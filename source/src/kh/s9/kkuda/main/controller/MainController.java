216cc45df672203992fa278dd3b300deee18abb3
qwe2733227 <72017540+qwe2733227@users.noreply.github.com>
2022-12-27 21:46:42 +0900

    상품목록 List<productVo> productList  ||  /main 부분 TODO

--- source/src/kh/s9/kkuda/main/controller/MainController.java
+++ source/src/kh/s9/kkuda/main/controller/MainController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kh.s9.kkuda.product.model.ProductService;
+
 @WebServlet("/main")
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,5 +20,9 @@ public class MainController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String ViewPath="WEB-INF/view/index.jsp";
 		request.getRequestDispatcher(ViewPath).forward(request, response);
+		
+		//TODO 메인페이지에 상품목록 나열하기
+		ProductService service = new ProductService();
+		
 	}
 }
