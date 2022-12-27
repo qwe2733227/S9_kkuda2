30e2be492d258054534194806262ed02ce33afb0
qwe2733227 <qwe2733227@naver.com>
2022-12-28 03:35:51 +0900

    sql 상품목록 insert

--- sql/S9.sql
+++ sql/S9.sql
@@ -57,5 +57,17 @@ REFERENCES PRODUCT (
 
 insert into member values('user1', 'user1', '성철', 1234, 980415, 'M');
 insert into member values('user2', 'user2', '하하', 1234, 980416, 'W');
+insert into product (POCKET, GOODSNAME, PRICE, PRODUCTIMG) 
+            values((Select nvl(max(pocket),0)+1 from PRODUCT), '삼성 노트북 갤럭시북(H) 15인치 i5 11세대 윈도우 10 Pro (NT761XDA-KR58D)',20000, 'https://contents.sixshop.com/thumbnails/uploadedFiles/16843/product/image_1664161070544_1000.png' );
+insert into product (POCKET, GOODSNAME, PRICE, PRODUCTIMG) 
+             values((Select nvl(max(pocket),0)+1 from PRODUCT), '아이패드 10세대 Wi-Fi 64GB 블루(MPQ13KH/A',38200, 'https://contents.sixshop.com/thumbnails/uploadedFiles/16843/product/image_1669015161739_1000.jpg' );
+insert into product (POCKET, GOODSNAME, PRICE, PRODUCTIMG) 
+             values((Select nvl(max(pocket),0)+1 from PRODUCT), '애플워치 SE GPS 40mm 실버 알루미늄 케이스, 화이트 스포츠밴드 (MNJV3KH/A)',20200, 'https://contents.sixshop.com/thumbnails/uploadedFiles/16843/product/image_1666784333665_1000.jpg' );
+insert into product (POCKET, GOODSNAME, PRICE, PRODUCTIMG) 
+             values((Select nvl(max(pocket),0)+1 from PRODUCT), '벤큐 게이밍 모니터 24.5인치 (XL2546K)',39400, 'https://contents.sixshop.com/thumbnails/uploadedFiles/16843/product/image_1647255441491_1000.png' );
+insert into product (POCKET, GOODSNAME, PRICE, PRODUCTIMG) 
+             values((Select nvl(max(pocket),0)+1 from PRODUCT), '에어팟 프로 2세대 (MQD83KH/A)',20200, 'https://contents.sixshop.com/thumbnails/uploadedFiles/16843/product/image_1666783190556_1000.jpg' );
+
+
 commit;
 desc member;
