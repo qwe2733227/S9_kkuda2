5fc21112463a9e6e74fa27c74ca98eddc672d1ec
qwe2733227 <qwe2733227@naver.com>
2022-12-28 03:36:09 +0900

    상품목록 /main

--- source/src/kh/s9/kkuda/main/controller/MainController.java
+++ source/src/kh/s9/kkuda/main/controller/MainController.java
@@ -1,6 +1,8 @@
 package kh.s9.kkuda.main.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kh.s9.kkuda.product.model.ProductService;
+import kh.s9.kkuda.product.model.ProductVo;
 
 @WebServlet("/main")
 public class MainController extends HttpServlet {
@@ -22,7 +25,10 @@ public class MainController extends HttpServlet {
 		request.getRequestDispatcher(ViewPath).forward(request, response);
 		
 		//TODO 메인페이지에 상품목록 나열하기
+		System.out.println("ProductList");
 		ProductService service = new ProductService();
+		List<ProductVo> voList = service.productList();
+		System.out.println("voList");
 		
 	}
 }
